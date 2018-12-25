83cb742e255097648fc2552b2e2f0888045443ee
Kamil Michna <kamil.michna28@gmail.com>
2018-12-25 20:12:46 +0100

    add alghoritm that sorts answers

--- getQuestion.js
+++ getQuestion.js
@@ -1,9 +1,18 @@
 const fs = require('fs');
 
-
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+        
+    }
+    return array;
+}
 function getQuestionFromFile(){
-    let data = fs.readFileSync('questions.json','utf-8');
-    return data;
+    let data = JSON.parse(fs.readFileSync('questions.json','utf-8'));
+    let dataToReturn = data.questions[Math.floor(Math.random()*data.questions.length)];
+    dataToReturn.answers = shuffleArray(dataToReturn.answers);
+    return dataToReturn
    
 }
 getQuestionFromFile();
